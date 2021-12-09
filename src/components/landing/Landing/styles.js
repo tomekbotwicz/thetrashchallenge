d22e341d8e6c568df80f7cdd1d45ba30d15b46fc
Tomek <46640406+botwiczt@users.noreply.github.com>
2021-12-08 19:07:26 -0500

    shirt still bugged out

--- src/components/landing/Landing/styles.js
+++ src/components/landing/Landing/styles.js
@@ -48,6 +48,7 @@ export const Chips = styled.div`
 
 export const Shirt = styled.div`
   transition: all 0.5s ease;
+  margin-left: 1rem;
   transform: rotate(5deg);
   cursor: pointer;
   img {
@@ -160,6 +161,39 @@ export const Wrap = styled.div`
   white-space: nowrap;
 `;
 
+export const Input = styled.input`
+display: inline-block;
+  font-size: 10pt;
+  padding: 8px;
+  background: #DCDCDC;
+  border: none;
+  border-radius: 10px 0 0 10px;
+  @media only screen and (max-width: 640px) {
+    font-size: 8pt;
+  }
+`;
+
+export const RegisterButton = styled.button`
+display: inline-block;
+  cursor: pointer;
+  font-size: 10pt;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  padding-left: 15px;
+  padding-right: 15px;
+  border: none;
+  border-radius: 0 10px 10px 0;
+  transition: all 0.5s ease;
+  background: #444444;
+  color: #FFF;
+  :hover {
+    box-shadow: ${({ theme }) => (theme === 'dark' ? 'inset 0 0 100px 100px #E5E5E5' : 'inset 0 0 100px 100px #444d56')};
+  }
+  @media only screen and (max-width: 640px) {
+    font-size: 8pt;
+  }
+`;
+
 export const Button = styled.button`
   display: inline-block;
   cursor: pointer;
@@ -184,37 +218,4 @@ export const Button = styled.button`
   @media only screen and (max-width: 407px) {
       border-radius: 10px;
     }
-`;
-
-export const RegisterButton = styled.button`
-display: inline-block;
-  cursor: pointer;
-  font-size: 10pt;
-  padding-top: 8px;
-  padding-bottom: 8px;
-  padding-left: 15px;
-  padding-right: 15px;
-  border: none;
-  border-radius: 0 10px 10px 0;
-  transition: all 0.5s ease;
-  background: #444444;
-  color: #FFF;
-  :hover {
-    box-shadow: ${({ theme }) => (theme === 'dark' ? 'inset 0 0 100px 100px #E5E5E5' : 'inset 0 0 100px 100px #444d56')};
-  }
-  @media only screen and (max-width: 640px) {
-    font-size: 8pt;
-  }
-`;
-
-export const Input = styled.input`
-display: inline-block;
-  font-size: 10pt;
-  padding: 8px;
-  background: #DCDCDC;
-  border: none;
-  border-radius: 10px 0 0 10px;
-  @media only screen and (max-width: 640px) {
-    font-size: 8pt;
-  }
 `;
